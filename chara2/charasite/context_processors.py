fc65670309c09176a88bb0682c4747f30bceb81b
XavierBdM <xavier.badin@gmail.com>
2017-10-28 13:54:54 +0200

    affichage comms
    Affichage des commentaires

--- chara2/charasite/context_processors.py
+++ chara2/charasite/context_processors.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 
 def listproj_context_processor(request):
     return {
-        'listproj': Project.objects.filter(is_published=True),
+        'listproj': Project.objects.filter(is_published=True).order_by('name'),
     }
 
 def listnewchapter_context_processor(request):
