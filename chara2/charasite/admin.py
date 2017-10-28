a7b0a3f1dafa114dbf80a46ce1bcfd3b2f3788e6
XavierBdM <xavier.badin@gmail.com>
2017-10-28 19:08:11 +0200

    free tuto
    On enlève le tuto

--- chara2/charasite/admin.py
+++ chara2/charasite/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import *#Author, Book, Genre, BookInstance, Project, TeamMember, Repository
+from .models import *
 
 #admin.site.register(Book)
 
@@ -10,33 +10,7 @@ from .models import *#Author, Book, Genre, BookInstance, Project, TeamMember, Re
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
-# Register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
-
 admin.site.register(Genre)
-#admin.site.register(BookInstance)
-
-# Register the Admin classes for Book using the decorator
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    pass
-
-# Register the Admin classes for BookInstance using the decorator
-
-@admin.register(BookInstance) 
-class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
-    list_filter = ('status', 'due_back')
-	
-    fieldsets = (
-        (None, {
-            'fields': ('book','imprint', 'id')
-        }),
-        ('Availability', {
-            'fields': ('status', 'due_back','borrower',)
-        }),
-    )
 admin.site.register(Project)
 admin.site.register(TeamMember)
 admin.site.register(Repository)
--- chara2/charasite/views.py
+++ chara2/charasite/views.py
@@ -27,11 +27,11 @@ def index(request):
     View function for home page of site.
     """
     # Generate counts of some of the main objects
-    num_books=Book.objects.all().count()
-    num_instances=BookInstance.objects.all().count()
+    num_books=Project.objects.all().count()
+    num_instances=Chapter.objects.all().count()
     # Available books (status = 'a')
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # The 'all()' is implied by default.
+    num_instances_available=Chapter.objects.all().count()
+    num_authors=User.objects.count()  # The 'all()' is implied by default.
     
     # Number of visits to this view, as counted in the session variable.
     num_visits=request.session.get('num_visits', 0)
@@ -87,92 +87,7 @@ def activate(request, uidb64, token):
         return render(request, 'account_activation_invalid.html')
 
 
-    
-class BookListView(generic.ListView):
-    model = Book
-    paginate_by = 10
 
-def BookDetailView(request,pk):
-    try:
-        book_id=Book.objects.get(pk=pk)
-    except Book.DoesNotExist:
-        raise Http404("Book does not exist")
-
-    #book_id=get_object_or_404(Book, pk=pk)
-    
-    return render(
-        request,
-        'charasite/book_detail.html',
-        context={'book':book_id,}
-    )
-    
-def AuthorDetailView(request,pk):
-    try:
-        author_id=Author.objects.get(pk=pk)
-    except Book.DoesNotExist:
-        raise Http404("Book does not exist")
-
-    #book_id=get_object_or_404(Book, pk=pk)
-    
-    return render(
-        request,
-        'charasite/author_detail.html',
-        context={'author':author_id,}
-    )
-    
-class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
-    """
-    Generic class-based view listing books on loan to current user. 
-    """
-    model = BookInstance
-    template_name ='charasite/bookinstance_list_borrowed_user.html'
-    paginate_by = 10
-    paginate_by = 10
-
-    def get_queryset(self):
-        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
-        
-        
-@permission_required('charasite.can_mark_returned')
-def renew_book_librarian(request, pk):
-    book_inst=get_object_or_404(BookInstance, pk = pk)
-
-    # If this is a POST request then process the Form data
-    if request.method == 'POST':
-
-        # Create a form instance and populate it with data from the request (binding):
-        form = RenewBookForm(request.POST)
-
-        # Check if the form is valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-            book_inst.due_back = form.cleaned_data['renewal_date']
-            book_inst.save()
-
-            # redirect to a new URL:
-            return HttpResponseRedirect(reverse('all_borrowed') )
-
-    # If this is a GET (or any other method) create the default form.
-    else:
-        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
-
-    return render(request, 'charasite/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
-    
-
-class AuthorCreate(CreateView):
-    model = Author
-    fields = '__all__'
-    initial={'date_of_death':'12/10/2016',}
-
-class AuthorUpdate(UpdateView):
-    model = Author
-    fields = ['first_name','last_name','date_of_birth','date_of_death']
-
-class AuthorDelete(DeleteView):
-    model = Author
-    success_url = reverse_lazy('index')
-    
 class ProjectListView(generic.ListView):
     model = Project
     paginate_by = 10
